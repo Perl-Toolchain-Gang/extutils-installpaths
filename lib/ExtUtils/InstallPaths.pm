5d35f79e1a76e161566fbcacf5806d3fa5e06c85
Leon Timmermans <fawaka@gmail.com>
2011-11-26 01:33:38 +0100

    Don't return references to internal state

--- lib/ExtUtils/InstallPaths.pm
+++ lib/ExtUtils/InstallPaths.pm
@@ -219,7 +219,7 @@ sub install_path {
 	my $self = shift;
 
 	my $map = $self->{install_path};
-	return $map unless @_;
+	return { %{$map} } unless @_;
 
 	my $type = shift;
 	Carp::croak('Type argument missing') unless defined $type ;
@@ -256,7 +256,7 @@ sub install_sets {
 		return $map->{$dirs}{$key};
 	}
 	elsif (defined $dirs) {
-		return $map->{$dirs};
+		return { %{ $map->{$dirs} } };
 	}
 	else {
 		Carp::croak('Can\'t determine installdirs for install_sets()');
@@ -291,7 +291,7 @@ sub install_base_relpaths {
 		$self->_set_relpaths($self->{install_base_relpaths}, @_);
 	}
 	my $map = $self->_merge_arglist($self->{install_base_relpaths}, $self->_default_base_relpaths);
-	return $map unless @_;
+	return { %{$map} } unless @_;
 	my $relpath = $map->{$_[0]};
 	return defined $relpath ? File::Spec->catdir( @$relpath ) : undef;
 }
@@ -307,7 +307,7 @@ sub prefix_relpaths {
 		$self->_set_relpaths($self->{prefix_relpaths}{$installdirs}, @_);
 	}
 	my $map = $self->_merge_arglist($self->{prefix_relpaths}{$installdirs}, $self->_default_prefix_relpaths->{$installdirs});
-	return $map unless @_;
+	return { %{$map} } unless @_;
 	my $relpath = $map->{$_[0]};
 	return defined $relpath ? File::Spec->catdir( @$relpath ) : undef;
 }
@@ -424,7 +424,7 @@ sub original_prefix {
 		$self->{original_prefix}{$key} = $value;
 	}
 	my $map = $self->_merge_arglist($self->{original_prefix}, $self->_default_original_prefix);
-	return $map unless defined $key;
+	return { %{$map} } unless defined $key;
 	return $map->{$key}
 }
 
