Perl-Toolchain-Gang/extutils-installpaths

commit e9209d214395eb95f38e9452b20650add2678f33

Try to install any installable paths

lib/ExtUtils/InstallPaths.pm
@@ -342,7 +342,7 @@ sub install_map {
 
 	my (%map, @skipping);
 	foreach my $type (keys %localdir_for) {
-		next if not -e $localdir_for{$type};
+		next if not $self->is_default_installable($type);
 		if (my $dest = $self->install_destination($type)) {
 			$map{$localdir_for{$type}} = $dest;
 		} else {

commit 6db76adf7dc3d5afd2639fbcd55a80c443c2ca3a

Restore installing non-standard types
This was broken by e9209d21

lib/ExtUtils/InstallPaths.pm
@@ -344,7 +344,7 @@ sub install_map {
 
 	my (%map, @skipping);
 	foreach my $type (keys %localdir_for) {
-		next if not $self->is_default_installable($type);
+		next if not -e $localdir_for{$type} and not $self->is_default_installable($type);
 		if (my $dest = $self->install_destination($type)) {
 			$map{$localdir_for{$type}} = $dest;
 		} else {